897c1e7a75e8c07363d66f1143a3e8ea576645b2
Casey Jak <casey.j.lee@gmail.com>
2018-08-15 15:01:12 -0500

    css reset

---
+++ assets/javascript/game.js
@@ -0,0 +1,7 @@
+let wordBank = ["Navi", "Mastersword", "Triforce", "Zelda", "Link", "Hyrule", 
+                "Ganon", "Midna", "Tinkle", "Fi", "Power", "Courage", "Wisdom"];
+
+let randomWord = wordBank[Math.floor(Math.random()*wordBank.length)];
+
+console.log(wordBank)
+console.log(randomWord)
