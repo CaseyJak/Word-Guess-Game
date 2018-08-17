3f2fe69580ff8a522e3d0ef22e6d4eb7cb20c364
Casey Jak <casey.j.lee@gmail.com>
2018-08-17 01:50:34 -0500

    first game loop attempt

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,65 @@
+
+
+
 let wordBank = ["Navi", "Mastersword", "Triforce", "Zelda", "Link", "Hyrule", 
-                "Ganon", "Midna", "Tinkle", "Fi", "Power", "Courage", "Wisdom"];
+                "Ganon", "Midna", "Tinkle", "Fi", "Power", "Courage", "Wisdom"];              
 
-let randomWord = wordBank[Math.floor(Math.random()*wordBank.length)];
+function setGame(){
 
-console.log(wordBank)
-console.log(randomWord)
+    let chances = 6;
+
+    let wrong = [];
+
+    // Computer Chooses Random Word
+
+    let randomWord = wordBank[Math.floor(Math.random()*wordBank.length)];
+
+    let gameAnswer = randomWord.split("");
+
+    let blankAnswer = [];
+        for (let i=0; i < gameAnswer.length; i++) {
+        blankAnswer.push("_");
+    }
+
+    // User Game Loop
+
+    let alpha = ["a", "b", "c", "d", "e", "f", "g", 
+    "h", "i", "k", "l","m", "n", "o", "p", "q", "r", 
+    "s", "t", "u", "v", "w", "x", "y", "z"]
+
+    document.oneupkey = function playerGuesing (event) {
+        let userGuess = event.key;
+        if (userGuess !== alpha){                       //if user presses a non letter key
+            alert("invalid selection")                  //tells player it's an invalid selection
+        } 
+        else if (userGuess === alpha){               //if user presses a letter key
+        for (let i=0; i<gameAnswer.length;i++)       //checks to see if player choose a correct letter
+        if (userGuess === gameAnswer[i]) {           //if user guesses correct
+            blankAnswer[i] = gameAnswer[i];          //replace blank space with correct letter
+            if (gameAnswer = blankAnswer) {          //if player has all the letters
+                alert("You win!");                   //user wins
+                setGame();                           // resets game
+            }
+        }
+        else if (userGuess !== gameAnswer[i]){      //if user guess wrong
+            chances--;                              //player counter goes down by 1
+            wrong.push(userGuess);
+            if (chances < 1) {                      //if player runs out of live
+                alert("You lose!");                 //player loess
+                setGame();                          //resets game
+            }
+        }            
+    }
+    }
+    console.log("gameAnswer: " + gameAnswer);
+    console.log("randomWord: " + randomWord);
+    console.log("blankAnswer: " + blankAnswer);
+    }
+
+        /* maybe for game play 
+    let playersGuess=
+
+    if(gameWord) */
+
+
+
